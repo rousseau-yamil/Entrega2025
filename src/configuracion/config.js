1ca1f091d73e316ca658f18fd706ec07efe3c7c3
rousseau-yamil <95605438+rousseau-yamil@users.noreply.github.com>
2025-07-09 11:51:38 -0300

    Add files via upload

---
+++ src/configuracion/config.js
@@ -0,0 +1,23 @@
+// export const config={
+//     PORT:3000,
+//     //MONGO_PORT:27017,
+
+//     //MONGO_URL:"mongodb+srv://coderhouse:codercoder2023@cluster0.wpxpupc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+//     MONGO_URL:'mongodb://127.0.0.1:27017/',
+//     DB_NAME:'entrega-final',
+//     SECRET_SESSION:"mi_clave_super_secreta",
+// }
+//mongodb+srv://<db_username>:<db_password>@cluster0.alfrzlc.mongodb.net/
+
+export const config = {
+  MONGO_HOST: '127.0.0.1',
+  MONGO_PORT: 27017,
+  DB_NAME: 'entrega-final',
+
+  get MONGO_URL() {
+    // URL completa con base de datos incluida
+    return `mongodb://${this.MONGO_HOST}:${this.MONGO_PORT}/${this.DB_NAME}`;
+  },
+
+  SESSION_SECRET: 'mi_clave_super_secreta'
+};
